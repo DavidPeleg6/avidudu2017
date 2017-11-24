8250aea7759c49677a19aa0ecac55c57bb9092c5
Avihai Didi <avihai.didi@gmail.com>
2017-11-24 19:31:23 +0200

    ai player placeholder addition

---
+++ PlayerAI.h
@@ -0,0 +1,17 @@
+/*
+ * PlayerAI.h
+ *
+ *  Created on: Nov 24, 2017
+ *      Author: xu
+ */
+
+#ifndef PLAYERAI_H_
+#define PLAYERAI_H_
+
+class PlayerAI {
+public:
+	PlayerAI();
+	virtual ~PlayerAI();
+};
+
+#endif /* PLAYERAI_H_ */

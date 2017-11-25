3c69a7d97a3eef45531fc85415da18ddd19632f6
David Peleg <dudugeda@gmail.com>
2017-11-25 11:31:29 +0200

    new main function asking for AI or human

--- main.cpp
+++ main.cpp
@@ -12,22 +12,36 @@
 #include "Rules.h"
 #include "Game.h"
 using namespace std;
+
+#define AI 1
+#define HUMAN 2
 /**
  * Main function of the program.
  */
 int main() {
 	Board* b = new Board(8, 8);
 	b->SetUpGame();
-	ConsoleDisplay* d = new ConsoleDisplay();
+	Display* d = new ConsoleDisplay();
 	Rules* r = new Rules(b);
-	PlayerAI* p1 = new PlayerAI(r, b, 1);
-	PlayerAI* p2 = new PlayerAI(r, b, 2);
-	Game* g = new Game(b, r, p1, p2);
+	Player *p[2];
+	int player;
+	for(int i = 1; i <= 2; i++) {
+		d->AskForPlayer(i);
+		cin >> player;
+		switch (player) {
+		case AI:
+			p[i - 1] = new PlayerAI(r, b, i);
+			break;
+		case HUMAN:
+			p[i - 1] = new PlayerHumanLocal();
+		}
+	}
+	Game* g = new Game(b, r, p[0], p[1]);
 	g->RunGame(d);
 	delete b;
 	delete d;
-	delete p1;
-	delete p2;
+	delete p[0];
+	delete p[1];
 	delete r;
 	delete g;
 	return 0;

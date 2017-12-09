67b37a13126ca54d296d4dca308b3d16de5f39a9
Avihai Didi <avihai.didi@gmail.com>
2017-12-09 18:09:59 +0200

    some minor meaningless crap

--- ServerEx4/Server.cpp
+++ ServerEx4/Server.cpp
@@ -64,7 +64,9 @@ void Server::start() {
 		close(client2Socket);
 	}
 }
-
+/*
+ * Manages the data transfer between two clients playing a game.
+ */
 void Server::handleClients(int client1Socket, int client2Socket) {
 	//send a single digit to the clients (first = 1, second = 2)
 	if(!startGame(client1Socket, client2Socket)) {
@@ -113,7 +115,12 @@ void Server::handleClients(int client1Socket, int client2Socket) {
 	}
 	return;
 }
-
+/*
+ * Tells each client what color they are.
+ * @param clientSocket1 - the socket being used by the first client.
+ * @param clientSocket2 - the socket being used by the second client.
+ * @return - error code if an error happened.
+ */
 int Server::startGame(int client1Socket, int client2Socket) {
 	int turn = 2;
 	int n = write(client1Socket, &turn, sizeof(turn));
@@ -129,7 +136,11 @@ int Server::startGame(int client1Socket, int client2Socket) {
 	}
 	return 1;
 }
-
+/*
+ * Gets a move from a client.
+ * @param clientSocket - the socket being used by the client in question.
+ * @return - error code if an error happened.
+ */
 int Server::getMove(int clientSocket) {
 	int n = read(clientSocket, &move[0], sizeof(move[0]));
 	if(n == -1) {
@@ -151,23 +162,42 @@ int Server::getMove(int clientSocket) {
 	}
 	return 1;
 }
-
+/*
+ * Sends a move to a client.
+ * @param clientSocket - the socket being used by the client in question.
+ * @return - error code if an error happened.
+ */
 int Server::passMove(int clientSocket) {
-	int n = write(clientSocket, &move[0], sizeof(move[0]));
+	int n;
+	try {
+		n = write(clientSocket, &move[0], sizeof(move[0]));
+	} catch (const char *msg) {
+		return ERROR;
+	}
 	if(n == -1) {
 		return ERROR;
 	}
-	n = write(clientSocket, &move[1], sizeof(move[1]));
+	try {
+		n = write(clientSocket, &move[1], sizeof(move[1]));
+	} catch (const char *msg) {
+		return ERROR;
+	}
 	if(n == -1) {
 		return ERROR;
 	}
-	n = write(clientSocket, &move[2], sizeof(move[2]));
+	try {
+		n = write(clientSocket, &move[2], sizeof(move[2]));
+	} catch (const char *msg) {
+		return ERROR;
+	}
 	if(n == -1) {
 		return ERROR;
 	}
 	return 1;
 }
-
+/*
+ * Shuts the server down.
+ */
 void Server::stop() {
 	close(serverSocket);
 }
--- ServerEx4/main.cpp
+++ ServerEx4/main.cpp
@@ -8,7 +8,9 @@
 #include "headersS/FileReader.h"
 #include <stdlib.h>
 #define DATAFILE "ipfile"
-
+/*
+ * Starts the server and handles the clients.
+ */
 int main() {
 	FileReader* reader = new FileReader(DATAFILE);
 	int* data = reader->read();
